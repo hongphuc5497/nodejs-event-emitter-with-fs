5b56d78f55e9e15e4b3d4a8e2fcd1d88d68d9456
Hong Phuc <hongphuc.dthp@gmail.com>
2022-10-20 17:05:49 +0700

    feat: multiple events

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
 const app = require('express')();
 const EventEmitter = require('node:events');
+const fs = require('fs');
 
 const myEmitter = new EventEmitter();
-myEmitter.on('call', () => {
-  console.log('Calling emitter')
-})
 
-app.get('/', (req, res) => {
-  myEmitter.emit('call')
-  res.status(200).json({
-    message: 'test'
-  })
+// Read
+myEmitter.on('read', (cb) => {
+	const EXAMPLE_FILE = 'lorem.txt';
+	fs.readFile(`${__dirname}/${EXAMPLE_FILE}`, (err, data) => {
+		if (err) console.error(err);
+
+		myEmitter.emit('show', data);
+
+		// cb(data);
+	});
+});
+
+// Show
+myEmitter.on('show', (data) => {
+	console.log('===========');
+	console.log(data);
+	console.log('===========');
+});
+
+app.get('/', async (req, res) => {
+	myEmitter.emit('read');
+	// const data = await new Promise(resolve => {
+	// })
+	// console.log('sss', data)
+
+	res.status(200).json({
+		message: 'test',
+	});
+	// res.status(200).json(data.toJSON());
 });
 
 const PORT = process.env.PORT || 8888;
